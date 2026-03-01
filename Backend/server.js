0012a1237afc2658e016810d4facf22d513879a0
saikrishna7288 <padakantisaikrishna0@gmail.com>
2026-03-01 22:53:55 +0530

    port updated

--- Backend/server.js
+++ Backend/server.js
@@ -7,14 +7,15 @@ import authRoutes from "./routes/authRoutes.js";
 import convertRoutes from "./routes/convertRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
@@ -30,13 +31,13 @@ app.get("/", (req, res) => {
 });
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Atlas Connected ✅");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
-
-
