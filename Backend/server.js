c5ec2d3705dbdce530c490222f384b8323b66162
saikrishna7288 <padakantisaikrishna0@gmail.com>
2026-03-02 23:40:26 +0530

    updated eror

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chronomind-eight.vercel.app",
     credentials: true,
   }),
 );
